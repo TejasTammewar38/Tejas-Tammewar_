52425ee5ce5526e1abbb8b01e853771b41256125
TejasTammewar38 <tammewartejas11@gmail.com>
2026-03-30 23:16:14 +0530

    Implement multiplication program in C++

---
+++ multiplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a, b, result;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+
+    result = a * b;
+
+    std::cout << "Multiplication result: " << result << std::endl;
+
+    return 0;
+}
